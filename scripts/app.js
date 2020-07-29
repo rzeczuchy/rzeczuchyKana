02761a588be765f1a8f9797ff1b95212392db133
rzeczuchy <57368722+rzeczuchy@users.noreply.github.com>
2020-07-29 17:11:12 +0200

    Fetching dispay and form elements in ts

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+"use strict";
+var kanaDisplay = (document.getElementById("kanaDisplay"));
+var kanaForm = (document.getElementById("kanaForm"));
+window.onload = function () {
+    initialize();
+};
+var initialize = function () { };
---
+++ scripts/app.ts
@@ -0,0 +1,14 @@
+"use strict";
+
+const kanaDisplay: HTMLDivElement = <HTMLDivElement>(
+  document.getElementById("kanaDisplay")
+);
+const kanaForm: HTMLFormElement = <HTMLFormElement>(
+  document.getElementById("kanaForm")
+);
+
+window.onload = (): void => {
+  initialize();
+};
+
+const initialize = (): void => {};
