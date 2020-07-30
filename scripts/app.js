9f7a5dee0230e213e0b40a9590aba7d952f266d7
rzeczuchy <57368722+rzeczuchy@users.noreply.github.com>
2020-07-30 10:06:18 +0200

    Adding basic e2e functionality

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,83 @@
 "use strict";
+// CONSTANTS, VARIABLES
 var kanaDisplay = (document.getElementById("kanaDisplay"));
-var kanaForm = (document.getElementById("kanaForm"));
+var answer = (document.getElementById("answer"));
+var feedback = (document.getElementById("feedback"));
+var hiragana;
+var katakana;
+var displayedSyllable;
+var attempts;
+// EVENTS
 window.onload = function () {
     initialize();
 };
-var initialize = function () { };
+// CLASSES
+var Syllable = /** @class */ (function () {
+    function Syllable(kana, romaji) {
+        this.kana = kana;
+        this.romaji = romaji;
+    }
+    return Syllable;
+}());
+// FUNCTIONS
+var initialize = function () {
+    populateHiragana();
+    populateKatakana();
+    reset();
+};
+var reset = function () {
+    attempts = 0;
+    answer.value = "";
+    feedback.innerHTML = "Click submit to check.";
+    displayRandom();
+};
+var checkAnswer = function () {
+    attempts++;
+    if (displayedSyllable.romaji == answer.value) {
+        feedback.innerHTML = "Correct!";
+    }
+    else if (attempts < 3) {
+        feedback.innerHTML = "Wrong - try again!";
+    }
+    else {
+        feedback.innerHTML =
+            "Wrong - correct answer is [" + displayedSyllable.romaji + "].";
+    }
+};
+var displayRandom = function () {
+    displayedSyllable = getRandomSyllable();
+    if (displayedSyllable != null) {
+        kanaDisplay.innerHTML = displayedSyllable.kana;
+    }
+};
+var getRandomSyllable = function () {
+    switch (randomNumber(0, 1)) {
+        case 0:
+            return hiragana[randomNumber(0, hiragana.length - 1)];
+        case 1:
+            return katakana[randomNumber(0, katakana.length - 1)];
+        default:
+            return null;
+    }
+};
+var populateHiragana = function () {
+    hiragana = [
+        new Syllable("あ", "a"),
+        new Syllable("い", "i"),
+        new Syllable("う", "u"),
+        new Syllable("え", "e"),
+        new Syllable("お", "o"),
+    ];
+};
+var populateKatakana = function () {
+    katakana = [
+        new Syllable("ア", "a"),
+        new Syllable("イ", "i"),
+        new Syllable("ウ", "u"),
+        new Syllable("エ", "e"),
+        new Syllable("オ", "o"),
+    ];
+};
+var randomNumber = function (min, max) {
+    return Math.floor(Math.random() * (+max + 1 - +min) + +min);
+};
--- scripts/app.ts
+++ scripts/app.ts
@@ -1,14 +1,102 @@
 "use strict";
 
+// CONSTANTS, VARIABLES
 const kanaDisplay: HTMLDivElement = <HTMLDivElement>(
   document.getElementById("kanaDisplay")
 );
-const kanaForm: HTMLFormElement = <HTMLFormElement>(
-  document.getElementById("kanaForm")
+const answer: HTMLTextAreaElement = <HTMLTextAreaElement>(
+  document.getElementById("answer")
 );
+const feedback: HTMLDivElement = <HTMLDivElement>(
+  document.getElementById("feedback")
+);
+let hiragana: Syllable[];
+let katakana: Syllable[];
+let displayedSyllable: Syllable;
+let attempts: number;
 
+// EVENTS
 window.onload = (): void => {
   initialize();
 };
 
-const initialize = (): void => {};
+// CLASSES
+class Syllable {
+  kana: string;
+  romaji: string;
+
+  constructor(kana: string, romaji: string) {
+    this.kana = kana;
+    this.romaji = romaji;
+  }
+}
+
+// FUNCTIONS
+const initialize = (): void => {
+  populateHiragana();
+  populateKatakana();
+  reset();
+};
+
+const reset = (): void => {
+  attempts = 0;
+  answer.value = "";
+  feedback.innerHTML = "Click submit to check.";
+  displayRandom();
+};
+
+const checkAnswer = (): void => {
+  attempts++;
+
+  if (displayedSyllable.romaji == answer.value) {
+    feedback.innerHTML = "Correct!";
+  } else if (attempts < 3) {
+    feedback.innerHTML = "Wrong - try again!";
+  } else {
+    feedback.innerHTML =
+      "Wrong - correct answer is [" + displayedSyllable.romaji + "].";
+  }
+};
+
+const displayRandom = (): void => {
+  displayedSyllable = getRandomSyllable();
+
+  if (displayedSyllable != null) {
+    kanaDisplay.innerHTML = displayedSyllable.kana;
+  }
+};
+
+const getRandomSyllable = (): Syllable => {
+  switch (randomNumber(0, 1)) {
+    case 0:
+      return hiragana[randomNumber(0, hiragana.length - 1)];
+    case 1:
+      return katakana[randomNumber(0, katakana.length - 1)];
+    default:
+      return null;
+  }
+};
+
+const populateHiragana = (): void => {
+  hiragana = [
+    new Syllable("あ", "a"),
+    new Syllable("い", "i"),
+    new Syllable("う", "u"),
+    new Syllable("え", "e"),
+    new Syllable("お", "o"),
+  ];
+};
+
+const populateKatakana = (): void => {
+  katakana = [
+    new Syllable("ア", "a"),
+    new Syllable("イ", "i"),
+    new Syllable("ウ", "u"),
+    new Syllable("エ", "e"),
+    new Syllable("オ", "o"),
+  ];
+};
+
+const randomNumber = (min, max) => {
+  return Math.floor(Math.random() * (+max + 1 - +min) + +min);
+};
